60d641869540a42fb08cd9c3e21127bb76be2d83
yurem <Yuriy.Movchan@gmail.com>
2022-02-05 10:55:44 +0300

    chore: add BC key generation utils, update IDP to 4.1.5

---
+++ shib-oxauth-authn/src/main/java/org/gluu/idp/utilities/java/support/security/BasicKeystoreKeyStrategyTool.java
@@ -0,0 +1,21 @@
+package org.gluu.idp.utilities.java.support.security;
+
+import javax.annotation.Nonnull;
+
+import org.gluu.oxauth.model.util.SecurityProviderUtility;
+
+/**
+ * Init BC/BCFIPS before invoking tool
+ *
+ * @author Yuriy Movchan Date: 02/05/2022
+ */
+public class BasicKeystoreKeyStrategyTool
+		extends net.shibboleth.utilities.java.support.security.BasicKeystoreKeyStrategyTool {
+
+	public static void main(@Nonnull final String[] args) throws Exception {
+		SecurityProviderUtility.installBCProvider(true);
+
+		net.shibboleth.utilities.java.support.security.BasicKeystoreKeyStrategyTool.main(args);
+	}
+
+}
