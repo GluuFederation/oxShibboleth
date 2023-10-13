915f21d1ca426526281a4d0740a99acb5b721f41
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2023-10-13 18:29:14 +0300

    Fix serialization after deserialization

--- spring-flow/src/main/java/org/springframework/webflow/engine/impl/FlowSessionImpl.java
+++ spring-flow/src/main/java/org/springframework/webflow/engine/impl/FlowSessionImpl.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -157,7 +157,7 @@ class FlowSessionImpl implements FlowSession, Externalizable {
 	}
 
 	public void writeExternal(ObjectOutput out) throws IOException {
-		out.writeObject(flow != null ? flow.getId() : null);
+		out.writeObject(flow != null ? flow.getId() : flowId);
 		out.writeObject(state != null ? state.getId() : null);
 		out.writeObject(scope);
 		out.writeObject(parent);
